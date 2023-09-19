0154e7a5216c0e24181d684215b9174585c4ae47
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-09-19 14:27:48 -0300

    Feat: Task component 1/2

---
+++ src/components/Task.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
+import commonStyles from '../commonStyles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+export default props => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.checkContainer}>{getCheckView()}</View>
+      <View>
+        <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
+        <Text style={styles.date}>{formattedDate}</Text>
+      </View>
+    </View>
+  );
+};
+
+function getCheckView(doneAt) {
+  if (doneAt != null) {
+    return (
+      <View style={styles.done}>
+        <Icon name="check" size={20} color="#FFF" />
+      </View>
+    );
+  } else {
+    return <View style={styles.pending} />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderColor: '#AAA',
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    paddingVertical: 10,
+    backgroundColor: '#FFF',
+  },
+  checkContainer: {
+    width: '20%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pending: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: '#555',
+  },
+  done: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    backgroundColor: '#4D7031',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  desc: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.mainText,
+    fontSize: 15,
+  },
+  date: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.subText,
+    fontSize: 12,
+  },
+  right: {
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+  },
+  left: {
+    flex: 1,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  excludeIcon: {
+    marginLeft: 10,
+  },
+  excludeText: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
+    margin: 10,
+  },
+});
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -5,7 +5,7 @@ import todayImage from '../../assets/imgs/today.jpg';
 import moment from 'moment';
 import 'moment/locale/pt-br';
 import commonStyles from '../commonStyles';
-
+import Task from '../components/Task';
 export default class TaskList extends Component {
   render() {
     const today = moment()
@@ -21,9 +21,11 @@ export default class TaskList extends Component {
           </View>
         </ImageBackground>
         <View style={styles.taskList}>
-          <Text>Tarefa #01</Text>
-          <Text>Tarefa #02</Text>
-          <Text>Tarefa #03</Text>
+          <Task
+            desc="Comprar Livro"
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
         </View>
       </View>
     );
