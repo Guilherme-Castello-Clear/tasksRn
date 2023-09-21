18b8816af18dff28dd6ad823a926f80314dc8ff9
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-09-21 07:56:06 -0300

    Feat: Signin and signup

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -11,14 +11,20 @@ import {
 import AuthInput from '../components/AuthInput';
 import backgroundImage from '../../assets/imgs/login.jpg';
 import commonStyles from '../commonStyles';
+import {server, showError, showSuccess} from '../common';
 
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  stageNew: false,
+};
+
+import axios from 'axios';
 export default class Auth extends Component {
   state = {
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    stageNew: false,
+    ...initialState,
   };
 
   signinOrSignup = () => {
@@ -26,6 +32,37 @@ export default class Auth extends Component {
       this.signup();
     } else {
       this.signin();
+    }
+  };
+
+  signup = async () => {
+    try {
+      await axios.post(`${server}/signup`, {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+        confirmPassword: this.state.confirmPassword,
+      });
+      showSuccess('Usuário cadastro!');
+      this.setState({...initialState});
+    } catch (e) {
+      showError(e);
+    }
+  };
+
+  signin = async () => {
+    try {
+      const res = await axios.post(`${server}/signin`, {
+        email: this.state.email,
+        password: this.state.password,
+      });
+
+      axios.defaults.headers.common['Authorization'] = `bearer ${
+        res.data.token
+      }`;
+      this.props.navigation.navigate('Home');
+    } catch (e) {
+      showError(e);
     }
   };
 
