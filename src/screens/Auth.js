206c24e1c578aec39bb197bc08146b871e73b5c8
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-09-21 07:45:17 -0300

    Feat: Added authInput

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   Platform,
 } from 'react-native';
+import AuthInput from '../components/AuthInput';
 import backgroundImage from '../../assets/imgs/login.jpg';
 import commonStyles from '../commonStyles';
 
