d7c917fce64405e19a09795fbe7afaedfd6bc36d
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-09-21 07:29:09 -0300

    Feat: Auth screen 2/3

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,13 +1,53 @@
 import React, {Component} from 'react';
-import {ImageBackground, Text, StyleSheet} from 'react-native';
+import {
+  ImageBackground,
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 import backgroundImage from '../../assets/imgs/login.jpg';
 import commonStyles from '../commonStyles';
 
 export default class Auth extends Component {
+  state = {
+    name: '',
+    email: '',
+  };
+
   render() {
     return (
       <ImageBackground source={backgroundImage} style={styles.background}>
         <Text style={styles.title}>Tasks</Text>
+        <View style={styles.formContainer}>
+          <Text style={styles.subtitle}>
+            {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados'}
+          </Text>
+          <AuthInput
+            icon="at"
+            placeholder="E-mail"
+            value={this.state.email}
+            style={styles.input}
+            onChangeText={email => this.setState({email})}
+          />
+          <AuthInput
+            icon="lock"
+            placeholder="Senha"
+            value={this.state.password}
+            style={styles.input}
+            secureTextEntry={true}
+            onChangeText={password => this.setState({password})}
+          />
+          <TouchableOpacity onPress={this.signinOrSignup} disabled={!validForm}>
+            <View>
+              <Text style={styles.buttonText}>
+                 Entrar
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
     );
   }
@@ -25,5 +65,33 @@ const styles = StyleSheet.create({
     color: commonStyles.colors.secondary,
     fontSize: 70,
     marginBottom: 10,
+  },
+  subtitle: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  formContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    padding: 20,
+    width: '90%',
+  },
+  input: {
+    marginTop: 10,
+    backgroundColor: '#FFF',
+  },
+  button: {
+    backgroundColor: '#080',
+    marginTop: 10,
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 7,
+  },
+  buttonText: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
   },
 });
